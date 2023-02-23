0755e8afd10fd29d0e428327fb4b761aef56e61a
Jonathan Carlos Costa <40038663+jothank@users.noreply.github.com>
2023-02-23 19:48:27 -0300

    Create aula.r

---
+++ aula.r
@@ -0,0 +1,34 @@
+matrix(c(1:3, 8),2 ) #criar matrix
+
+matrix d = c(832,769,510,568) #vetor
+
+d e = c(100,200,300,400) #vetor
+
+e z = cbind(d,e) #vetor em coluna
+
+z w = rbind(d,e) #vetor em tabela 
+
+w summary(d) #sumario do vetor e = rep("teste",5) #repeticao
+
+e h = rep(6,20) #repeticao
+
+h t = seq(3,40,3) #sequencia primeiro inicio, total, pular
+t 
+
+Data = rnorm(1000, mean = 0, sd = 2)
+
+hist(Data)
+
+v = c(30,10,39,56,78)
+v
+
+max(v) #maximo do vetor 
+
+min(v) #minimo do vetor 
+
+length(v) #quantidade dentro do vetor 
+
+sum(v) #soma do vetor 
+
+
+cumsum(v) #passo a passo da soma do vetor
