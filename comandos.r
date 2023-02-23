58b305b155eebcbd138e37e1e129176570321ed8
Jonathan Carlos Costa <40038663+jothank@users.noreply.github.com>
2023-02-23 20:48:24 -0300

    Create comandos.r

---
+++ comandos.r
@@ -0,0 +1,15 @@
+print(c(sqrt(749)))
+
+g = 100.00
+g
+
+ls.str()
+
+cat(class(g))
+
+.name = "Jonathan" #variavel oculta
+
+ls()
+
+print(ls(all.name = TRUE)) #exibida apenas utilizando all.name = TRUE com print e ls
+
