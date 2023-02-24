68dcf0e4cfeb52f2344ab4f6f302a391a55e92fc
Jonathan Carlos Costa <40038663+jothank@users.noreply.github.com>
2023-02-23 21:20:59 -0300

    Update comandos.r

--- comandos.r
+++ comandos.r
@@ -13,3 +13,54 @@ ls()
 
 print(ls(all.name = TRUE)) #exibida apenas utilizando all.name = TRUE com print e ls
 
+soma = 20 + 10;
+print("Soma");
+print(soma);
+
+print("Subtracao");
+print(30-10)
+
+print("Multiplicacao");
+print(30*10)
+
+print("Divisao");
+print(30/10)
+
+print("Potenciacao");
+print(30^2)
+
+print("Operador Mod");
+print(30%%7)
+
+if(20>10){
+  print("20 e maior que 10")  
+}
+
+if(10<20){
+  print("10 e menor que 20")  
+}
+
+if(20>=10){
+  print("20 e maior que 10")  
+}
+
+if(10<=20){
+  print("10 e menor que 20")  
+}
+
+if(10==10){
+  print("10 e igual a 10")  
+}
+
+if(20!=10){
+  print("20 e diferente de 10")  
+}
+
+nomes = c("Jonathan","Carlos","Costa","Thiago");
+
+nomes
+nomes[1]
+nomes[2]
+nomes[3]
+nomes[4]
+nomes[5]
