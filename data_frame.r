9aaab5ab1f5413078ea753f2e7be49c00ad4a373
Jonathan Carlos Costa <40038663+jothank@users.noreply.github.com>
2023-03-09 20:52:10 -0300

    Update data_frame.r

--- data_frame.r
+++ data_frame.r
@@ -16,3 +16,31 @@ cadastro = data.frame(
 
 # Exibe os Dados
 cadastro
+
+# Codigo do Registro
+codigoCadastro = 1L:4L
+
+# Modelo
+modelo <- c("Corolla", "Civic", "Golf", "Fiesta")
+
+# Fabricante
+fabricante <- c("Toyota", "Honda", "Volkswagen", "Ford")
+
+# Ano
+ano <- c(2017, 2018, 2019, 2016)
+
+# Valor
+valor <- c(25000, 27000, 22000, 18000)
+
+# Criar data.frame com o cadastro
+cadastro = data.frame(
+	Cod = codigoCadastro,		# Codigo do registro
+	Modelo = modelo,			# Modelo
+	Fabricante = fabricante, 	# Fabricante
+	Ano = ano,				# Ano
+	Valor = valor 			# Valor
+)
+
+# Exibe os Dados
+cadastro
+
