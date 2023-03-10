41c1f890cd6e5f32a3b9a73b21c598c49a04096a
Jonathan Carlos Costa <40038663+jothank@users.noreply.github.com>
2023-03-09 21:40:55 -0300

    Update manipulando_dataframe.r

--- manipulando_dataframe.r
+++ manipulando_dataframe.r
@@ -1,5 +1,5 @@
 # Codigo do Registro
-codigoCadastro = 1L:4L
+codigoCadastro = sample(x=1:100, size=4)
 
 # Modelo
 modelo <- c("Corolla", "Civic", "Golf", "Fiesta")
@@ -13,6 +13,9 @@ ano <- c(2017, 2018, 2019, 2016)
 # Valor
 valor <- c(25000, 27000, 22000, 18000)
 
+#
+revisado = "SIM"
+
 # Criar data.frame com o cadastro
 cadastro = data.frame(
 	Cod = codigoCadastro,		# Codigo do registro
@@ -25,10 +28,23 @@ cadastro = data.frame(
 # Exibe os Dados
 cadastro
 
-nrow(cadastro)
-ncol(cadastro)
+nrow(cadastro) # Exibe a quantidade de linhas
+ncol(cadastro) # Exibe a quantidade de colunas
 
 cadastro$Modelo # Pega pelo nome Variavel que recebeu
 cadastro[,4] # Pega a coluna
 cadastro[,"Valor"]
 cadastro[cadastro$Ano >= 2018, c("Cod","Modelo","Fabricante","Ano","Valor")]
+
+# Exibe X linhas do Inicio da tabela
+head(cadastro, n=2)
+# Exibe X linhas do Final da tabela
+tail(cadastro, n=2)
+
+cadastro$Cor = NA
+cadastro$Revisado = FALSE
+
+cadastro
+
+cadastro$Cor = NULL
+cadastro
